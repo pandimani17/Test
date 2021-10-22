dd13afc5834ed7fb672250e25702f77f63db1d3b
Pandi Mani <pandimani@oclocksoftware.co.in>
2021-10-22 16:38:22 +0530

    # Code check

--- BaseProject/assets/sql/1_create_schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS Data(
-  id INTEGER,
-  email TEXT,
-  first_name TEXT,
-  last_name TEXT
-);
--- BaseProject/lib/app/locator.config.dart
+++ BaseProject/lib/app/locator.config.dart
@@ -7,9 +7,8 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import '../core/data/api/apicall/dishesapicall.dart' as _i5;
-import '../core/data/api/restapi.dart' as _i4;
-import '../core/services/database_service.dart'
+import '../core/data/api/apicall/dishesapicall.dart' as _i4;
+import '../core/data/api/restapi.dart'
     as _i3; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
@@ -17,14 +16,7 @@ import '../core/services/database_service.dart'
 _i1.GetIt $initGetIt(_i1.GetIt get,
     {String environment, _i2.EnvironmentFilter environmentFilter}) {
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
-  final stackedServices = _$StackedServices();
-  gh.lazySingleton<_i3.DatabaseService>(() => stackedServices.databaseservice);
-  gh.singleton<_i4.RestApi>(_i4.RestApi());
-  gh.singleton<_i5.dishesapicall>(_i5.dishesapicall(get<_i4.RestApi>()));
+  gh.singleton<_i3.RestApi>(_i3.RestApi());
+  gh.singleton<_i4.dishesapicall>(_i4.dishesapicall(get<_i3.RestApi>()));
   return get;
-}
-
-class _$StackedServices extends _i3.StackedServices {
-  @override
-  _i3.DatabaseService get databaseservice => _i3.DatabaseService();
 }
